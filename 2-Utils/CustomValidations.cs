7c53c1901464bfaee9c14fb04efe17a1b37a0bfd
GalNeeman01 <zoritn@gmail.com>
2025-03-01 12:16:57 +0200

    Add user controller and service, add custom password validation, add encryptor class, add error messages for User model regex validations

---
+++ 2-Utils/CustomValidations.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Matrix;
+
+public class StrongPassword : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        // Fail if the input is invalid
+        if (value == null || value is not string)
+            return false;
+
+        string password = Convert.ToString(value)!;
+
+        // Fail if password is too short
+        if (password.Length < 8)
+            return false;
+
+        // Fail if: no numeric characters | no characters | non non-alphanumeric characters
+        if (!password.Any(c => char.IsUpper(c)) ||
+            !password.Any(c => char.IsDigit(c)) ||
+            !password.Any(c => !char.IsLetterOrDigit(c)))
+            return false;
+
+        return true;
+    }
+}
--- obj/Debug/net8.0/MatrixCollege.AssemblyInfo.cs
+++ obj/Debug/net8.0/MatrixCollege.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatrixCollege")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec50713c63890b63ecd68794116be3522d9e8fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a6c0a758327fe85278e1551b4243ce706ba6c1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatrixCollege")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatrixCollege")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
