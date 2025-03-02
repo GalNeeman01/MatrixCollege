7b74009869f31d9e5d75627bc9ee4af83c7cd7a2
GalNeeman01 <zoritn@gmail.com>
2025-03-02 02:21:43 +0200

    commit to pull merge

--- obj/Debug/net8.0/MatrixCollege.AssemblyInfo.cs
+++ obj/Debug/net8.0/MatrixCollege.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatrixCollege")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a6c0a758327fe85278e1551b4243ce706ba6c1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+301e8b76039be4b880e12b51c543762789f4c2ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatrixCollege")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatrixCollege")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
